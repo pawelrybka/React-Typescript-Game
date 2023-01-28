8e3ac05209c243e7c46a5b0ad2f8076cae736ff1
Pawel Rybka <rrybka1999@gmail.com>
2023-01-28 15:58:12 +0100

    Create button for addig new task

---
+++ src/components/addPoint/AddPoint.tsx
@@ -0,0 +1,13 @@
+import styles from './AddPoint.module.css'
+
+const AddPoint = () => {
+  return (
+    <button className={styles.button}>
+        Add new point
+    </button>
+  )
+}
+
+export default AddPoint
+
+
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/index.css'
 import '../styles/variables.css'
 import Footer from '../components/footer/footer'
 import Header from '../components/header/Header'
-
+import AddPoint from '@/components/addPoint/AddPoint'
 
 import type { AppProps } from 'next/app'
 
@@ -10,12 +10,10 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header/>
-        <Component {...pageProps} />
+      <Component {...pageProps} />
+      <AddPoint/>
       <Footer/>
     </>
-
-
-  ) 
-
-  
+  )
 }
+
