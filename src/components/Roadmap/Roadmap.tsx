7c326ba40fd11858fb89543593b4a364358876d4
Pawel Rybka <rrybka1999@gmail.com>
2023-01-30 11:02:01 +0100

    Roadmap layout create

---
+++ src/components/Roadmap/Roadmap.tsx
@@ -0,0 +1,14 @@
+import styles from './Roadmap.module.css'
+
+const Roadmap = () => {
+  return (
+    <div className={styles.roadmap}>
+        Roadmap
+    </div>
+  )
+}
+
+export default Roadmap
+
+
+
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,13 @@ import Footer from '../components/Footer/Footer'
 import Header from '../components/Header/Header'
 import ButtonSection from '../components/ButtonSection/ButtonSection'
 import Content from '../components/Content/Content'
+import Roadmap from '../components/Roadmap/Roadmap'
 
 const App = () => {
   return (
     <Content>
       <Header/>
+      <Roadmap/>
       <ButtonSection />
       <Footer/>
     </Content>
