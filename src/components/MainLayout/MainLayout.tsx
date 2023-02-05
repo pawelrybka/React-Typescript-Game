4a177c294c7f3d77812c72045bffd0638ef38dee
Pawel Rybka <rrybka1999@gmail.com>
2023-02-05 12:36:38 +0100

    Main layout

---
+++ src/components/MainLayout/MainLayout.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from 'react';
+import styles from './MainLayout.module.css';
+
+type MainLayoutProps = {
+	children: ReactNode;
+};
+
+const MainLayout = ({ children }: MainLayoutProps) => {
+	return <div className={styles.root}>{children}</div>;
+};
+
+export default MainLayout
---
+++ src/components/Roadmap/Roadmap.tsx
@@ -0,0 +1,12 @@
+import styles from './Roadmap.module.css'
+
+const Roadmap = () => {
+  return (
+    <div className={styles.roadmap}>
+        Roadmap
+    </div>
+  )
+}
+
+export default Roadmap
+
