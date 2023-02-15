8f2ceac3dd93f50e74e76b9053c5aa1a331e9b1d
Pawel Rybka <rrybka1999@gmail.com>
2023-02-15 22:16:25 +0100

    AddPointModal finish

--- src/components/AddPointModal/AddPointModal.tsx
+++ src/components/AddPointModal/AddPointModal.tsx
@@ -7,7 +7,7 @@ type AddPointModalProps = {
 
 const AddPointModal = ({active, setActive}: AddPointModalProps) => {
 
-  const tech = ["HTML", "CSS", "Javascript", "Typescript", "React", "Vue"]
+  const tech = ["HTML", "CSS", "Javascript", "Typescript", "React", "Vue", "Tailwind", "SASS"]
 
   const handleChange = () => setActive(!active)
 
@@ -28,9 +28,27 @@ const AddPointModal = ({active, setActive}: AddPointModalProps) => {
             tech.map(tech => <button>{tech}</button>)
           }
         </div>
-        <div>
-          
+        <div className={styles.timesection}>
+          <div className={styles.timesection__header}>
+            <span>Time to complete:</span>
+            <span>No time specified</span>
+          </div>
+          <div className={styles.timesection__buttons}>
+            <div>-1 Day</div>
+            <div>Days</div>
+            <div>+1 Day</div>
+            <div>-1 Week</div>
+            <div>Weeks</div>
+            <div>+1 Week</div>
+            <div>-1 Month</div>
+            <div>Months</div>
+            <div>+1 Month</div>
+            <div>-1 Year</div>
+            <div>Years</div>
+            <div>+1 Year</div>
+          </div>
         </div>
+        <button className={styles.confirm}>Confirm roadmap point</button>
       </div>
     </div>
   )
