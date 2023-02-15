7216682573abe70c1a7f44433271f1c53fc4c040
Pawel Rybka <rrybka1999@gmail.com>
2023-02-15 16:22:58 +0100

    Add button suggestions

--- src/components/AddPointModal/AddPointModal.tsx
+++ src/components/AddPointModal/AddPointModal.tsx
@@ -6,21 +6,27 @@ type AddPointModalProps = {
 }
 
 const AddPointModal = ({active, setActive}: AddPointModalProps) => {
-  
+
+  const tech = ["HTML", "CSS", "Javascript", "Typescript", "React"]
+
   const handleChange = () => setActive(!active)
 
   return (
-    <div className={`${styles.addPointModal} ${active ? styles.visible : ''}`}>
-      <div className={styles.addPointModal__container}>
-        <div className={styles.header}>
-          <span>JavaScript</span>
-          <button onClick={handleChange}>X</button>
-        </div>
-        <div className={styles.container}>
-          <form className={styles.form}>
-            <span>Add point</span>
-            <input type="text" />
-          </form>
+    <div className={`${styles.addpointmodal} ${active ? styles.visible : ''}`}>
+      <div className={styles.addpointmodal__header}>
+        <span>JavaScript</span>
+        <button onClick={handleChange}>X</button>
+      </div>
+      <div className={styles.addpointmodal__content}>
+        <form className={styles.form}>
+          <label>Roadmap point title</label>
+          <input type="text" />
+        </form>
+        <p>Suggestions:</p>
+        <div className={styles.buttons}>
+          {
+            tech.map(tech => <button>{tech}</button>)
+          }
         </div>
       </div>
     </div>
