2a5dd76e36d8ab5eb0ba2519af6ac99466afbe40
Pawel Rybka <rrybka1999@gmail.com>
2023-02-22 21:03:40 +0100

    Edit point modal

--- src/components/Point/Point.tsx
+++ src/components/Point/Point.tsx
@@ -1,12 +1,13 @@
 import styles from './Point.module.css'
 
-type props ={
-  elem: string
-}
+const Point = () => {
+  
+  const items = ['Item 1','Item 2','Item 3','Item 4','Item 5'];
 
-const Point = ({elem}: props) => {
+  const items2 = items.forEach(item => <button>{item}</button>)
+
   return (
-    <div className={styles.point}>{elem}</div>
+    <div className={styles.point}>{items}</div>
   )
 }
 
--- src/components/Roadmap/Roadmap.tsx
+++ src/components/Roadmap/Roadmap.tsx
@@ -1,20 +1,12 @@
 import styles from './Roadmap.module.css'
 import Point from '../Point/Point'
 
-const items = ['Item 1','Item 2','Item 3','Item 4','Item 5','sraka'];
-
-let itemList: JSX.Element[] = [];
-
-items.forEach((elem)=> itemList.push( <Point elem={elem}/> ))
-
 const Roadmap = () => {
   
   return (
     <div className={styles.roadmap}>
-      
-    
-      {itemList}
-      
+            
+      <Point/>
       
       {/* <p>No roadmap points</p> */}
       
