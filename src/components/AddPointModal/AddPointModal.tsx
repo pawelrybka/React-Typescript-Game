5d8bb8b31483f3b7e57b5fd8f7b8c756ade54082
Pawel Rybka <rrybka1999@gmail.com>
2023-03-01 13:36:08 +0100

    Changing style of AddroadmapPointModal

--- src/components/AddPointModal/AddPointModal.tsx
+++ src/components/AddPointModal/AddPointModal.tsx
@@ -38,42 +38,45 @@ const AddPointModal = ({ visible, setVisible }: AddPointModalProps) => {
       transition={{ duration: .3 }}
     >
       <div className={styles.addpointmodal__header}>
-        <span>JavaScript</span>
+        <h3>Add roadmap point</h3>
         <button onClick={() => setVisible(!visible)}><AiOutlineClose size={20}/></button>
       </div>
       <div className={styles.addpointmodal__content}>
-        <form className={styles.form}>
-          <label>Roadmap point title</label>
-          <input type="text" value={inputValue} onChange={handleInputChange}/>
-        </form>
-        <p>Suggestions:</p>
-        <div className={styles.buttons}>
-          {
-            tech.map(tech => <input type='button' value={tech} onClick={() => setInputValue(tech)}/>)
-          }
-        </div>
-        <div className={styles.timesection}>
-          <div className={styles.timesection__header}>
-            <span>Time to complete:</span>
-            <span>No time specified</span>
+        <div className={styles.panel}>
+          <form className={styles.form}>
+            <label>Roadmap point title</label>
+            <input type="text" value={inputValue} onChange={handleInputChange}/>
+          </form>
+          <p>Suggestions:</p>
+          <div className={styles.buttons}>
+            {
+              tech.map(tech => <input type='button' value={tech} onClick={() => setInputValue(tech)}/>)
+            }
           </div>
-          <div className={styles.timesection__buttons}>
-            <button>-1 Day</button>
-            <div>Days</div>
-            <button>+1 Day</button>
-            <button>-1 Week</button>
-            <div>Weeks</div>
-            <button>+1 Week</button>
-            <button>-1 Month</button>
-            <div>Months</div>
-            <button>+1 Month</button>
-            <button>-1 Year</button>
-            <div>Years</div>
-            <button>+1 Year</button>
+          <div className={styles.timesection}>
+            <div className={styles.timesection__header}>
+              <span>Time to complete:</span>
+              <span>No time specified</span>
+            </div>
+            <div className={styles.timesection__buttons}>
+              <button>-1 Day</button>
+              <div>Days</div>
+              <button>+1 Day</button>
+              <button>-1 Week</button>
+              <div>Weeks</div>
+              <button>+1 Week</button>
+              <button>-1 Month</button>
+              <div>Months</div>
+              <button>+1 Month</button>
+              <button>-1 Year</button>
+              <div>Years</div>
+              <button>+1 Year</button>
+            </div>
           </div>
         </div>
         <button className={styles.confirm} onClick={handleAddTodo}>Confirm roadmap point</button>
       </div>
+      
     </motion.div>
   )
 }
