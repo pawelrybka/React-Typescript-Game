a02cae80bd764d39db99889e91bd118f9dee5735
Pawel Rybka <rrybka1999@gmail.com>
2023-03-04 14:01:50 +0100

    Changing the footer text

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,10 @@
 import styles from './Footer.module.css'
-import { TfiHandPointRight, TfiHandPointLeft } from 'react-icons/tfi';
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
       <a href="https://github.com/pawelrybka/CodeRoad.git" target='_blank'>
-        <TfiHandPointRight size={40}/>
-        Check the code here!
-        <TfiHandPointLeft size={40}/>
+        <span >&lt; Check the code here! /&gt;</span>
       </a>
     </div>
   )
