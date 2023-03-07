ef5b2b0e3d58cf581ede137dd0241e39643b498f
Pawel Rybka <rrybka1999@gmail.com>
2023-03-07 13:14:07 +0100

    Change size of tech buttons

--- src/components/Backdrop/Backdrop.tsx
+++ src/components/Backdrop/Backdrop.tsx
@@ -1,20 +1,20 @@
 import React, { useContext } from 'react'
 import styles from './Backdrop.module.css'
 import Context from '../Context/Context'
-// import { motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 
 const Backdrop = () => {
 
   const{ alertVisible } = useContext(Context)
 
   return (
-    <div 
+    <motion.div 
       className={`${styles.backdrop} ${alertVisible ? styles.up : ''}`} 
-      // initial={{  opacity: 0 }}
-      // animate={{ opacity: 1 }}
-      // exit={{ opacity: 0 }}
-      // transition={{ duration: .3 }}
-    ></div>
+      initial={{  opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: .3 }}
+    ></motion.div>
   )
 }
 
