dfff01eb8d01ad2d30d955a48a1bd92961821644
Pawel Rybka <rrybka1999@gmail.com>
2023-04-26 20:55:39 +0200

    Add ESlint and Prettier into Project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,21 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'react/jsx-filename-extension': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/button-has-type': 0,
+    'import/no-unresolved': 0,
+    'no-shadow': 0,
   },
-}
+};
